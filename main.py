e548eadfdc06a962f1be5c55050aa9b02a9fda68
Anderson-silva <anderson.bhilly@gmail.com>
2023-11-03 09:17:48 -0300

    Versão um

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 
-# Escolhendo epsodios aleatorios da serie the big bang theory para assistir
+# Escolhendo epsodios aleatorios da serie the big bang theory para assistir.
 
 temporada = random.randint(1,12)
 
 epsodio = random.randint(1,20)
 
-print(f'O Episodio escolhido foi {epsodio} da temporada {temporada}')
+print(f'O Episodio escolhido foi: {epsodio} da temporada: {temporada}')
